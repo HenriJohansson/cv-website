5a9d4cb3acb0fa0c1adf8661f3e519bb19e586ca
Henri Johansson <henri.mm.johansson@gmail.com>
2024-01-12 17:44:33 +0200

    unused card div

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Cv from '@/components/Links/Cv'
+import Cv from '@/components/Links/CvLink'
 //import PaperCv from '@/components/CV/PaperCv'
 import './App.css'
 
@@ -6,10 +6,7 @@ function App() {
   return (
     <>
       <h1>My Name Is Henri Johansson</h1>
-      <div className="card">
-        <Cv/>
-      </div>
-      
+      <Cv/>
       <p className="about">
         This is the basic main page. 
       </p>
