b0be554d75ce7b01060d27ab99ea2a6c2e499d69
Henri Johansson <henri.mm.johansson@gmail.com>
2024-01-13 17:28:23 +0200

    Ignore configs with eslint, some vite sugar.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { fileURLToPath, URL } from "url";
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }: ConfigEnv) => void {
   plugins: [react()],
   build: {
     assetsDir: 'assets'
   },
+  base: '/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
