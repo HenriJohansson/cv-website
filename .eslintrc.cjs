fe261efbf2f25ef23d646a2ff3b1dbcd5306683b
Henri Johansson <henri.mm.johansson@gmail.com>
2024-01-19 09:40:48 +0200

    eslint should not interfere with workflows

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', '*.html', '*.config.ts'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.html', '*.config.ts', '/.github'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
