24077623222d377ca057f88707b34bd2754c1c33
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-05 11:43:55 +0200

    configuration changes

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { fileURLToPath } from 'url'
+import viteConfig from './vite.config'
+
+export default defineConfig(configEnv => mergeConfig(
+  viteConfig(configEnv),
+  defineConfig({
+    test: {
+      include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)"]
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@tests': fileURLToPath(new URL('./tests', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+        '@functions': fileURLToPath(new URL('./src/functions', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      },
+    }
+  })
+))
