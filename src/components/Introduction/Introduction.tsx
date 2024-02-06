eda473ffa89091a46408685ece2e6b1ec3bad7d7
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-06 20:59:09 +0200

    New Introduction component

---
+++ src/components/Introduction/Introduction.tsx
@@ -0,0 +1,22 @@
+import "@components/Header/header.css"
+import "@components/Introduction/introduction.css"
+
+export const Introduction = () => {
+  return (
+    <>
+      <section>
+        <div id="introduction">
+          <p>
+          I am a tech enthusiast
+          diving deep into the world of software development.
+          Currently studying Computer Science, I'm all about coding and creating cool stuff.
+          From back-end to front-end and everything in between,
+          I'm up for the challenge.
+          </p>
+        </div>
+      </section>
+      <span id="introduction_divider"></span>
+    </>
+  )
+}
+export default Introduction;
