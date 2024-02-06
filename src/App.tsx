34824ef384910ac177232ba368a30546debf0e06
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-06 20:59:38 +0200

    using introduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { Header } from '@components/Header/header'
 import '@/styles/app.css'
+import { ThemeProvider } from '@emotion/react';
+import theme from '@styles/muiColors';
+
 import Cv from '@pages/Cv/Cv';
 import Popup from "@components/util/Popup/popup";
 import NavBarManager from '@components/navigation/NavManager/NavManager';
 import PageInnitialLoad from '@components/PageInnitialLoad/PageInnitialLoad';
-import { ThemeProvider } from '@emotion/react';
-import theme from '@styles/muiColors';
+import Introduction from '@components/Introduction/Introduction';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <PageInnitialLoad></PageInnitialLoad>
       <Header></Header>
+      <Introduction></Introduction>
+
+      {/* Absolute elements defined on the bottom */}
       <NavBarManager></NavBarManager>
       <Popup id='Cv' enableClose={false} content={Cv} />
     </ThemeProvider>
