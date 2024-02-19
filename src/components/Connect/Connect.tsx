5a3bd348c6970843717012c18d31c8b440f8931b
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-19 15:53:44 +0200

    Connect component

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -1,7 +1,18 @@
+import HyperLink from "@components/Links/HyperLink";
+import { GitHub } from "@components/Icons/Github";
+import "@components/Connect/connect.css";
+import { LinkedIn } from "@components/Icons/LinkedIn";
+
 
 const Connect = () => {
   return (
-    <div>Connect</div>
+    <div className="connect">
+      <p>Connect with me!</p>
+      <div className="connectLinks">
+        <HyperLink href="https://github.com/HenriJohansson" target={true} abbreviationOrIcon={GitHub} ></HyperLink>
+        <HyperLink href="https://www.linkedin.com/in/henri-johansson-b24bb6237/" target={true} abbreviationOrIcon={LinkedIn} ></HyperLink>
+      </div>
+    </div>
   )
 }
 export default Connect;
