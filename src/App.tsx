24c724047d1a1aa34d305946bcd7b72e202d259c
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-19 15:51:59 +0200

    Added a pattern on to the page background

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ import theme from '@styles/muiColors';
 import Cv from '@pages/Cv/Cv';
 import Popup from "@components/util/Popup/popup";
 import NavBarManager from '@components/navigation/NavManager/NavManager';
-import PageInnitialLoad from '@components/PageInnitialLoad/PageInnitialLoad';
+//import PageInnitialLoad from '@components/PageInnitialLoad/PageInnitialLoad';
 import Introduction from '@components/Introduction/Introduction';
+import Connect from '@components/Connect/Connect';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <PageInnitialLoad></PageInnitialLoad>
+
+      <div className="bgPattern"></div>
       <Header></Header>
       <Introduction></Introduction>
+      <Connect></Connect>
 
       {/* Absolute elements defined on the bottom */}
       <NavBarManager></NavBarManager>
