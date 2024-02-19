f399ac151f3def127d2d8892126b035dce7f68fa
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-19 19:30:04 +0200

    css improvements, parsing improvement and color changes.

--- src/hooks/TextFetch.tsx
+++ src/hooks/TextFetch.tsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from "react";
 
 export const useFetchText = (str: string) => {
   const [text, setText] = useState("")
-  const parseEmoji = (str: string): string => {
-    const breakPoint: RegExp = /&#[0-9]+/gm;
-    return str.replace(breakPoint, s => String.fromCodePoint(+s.substring(2)));
-  }
+  const parser = new DOMParser();
   useEffect(() => {
   fetch(str)
     .then(value => value.text())
-    .then(value => setText(parseEmoji(value)));
+    .then(value => {
+      return setText(parser.parseFromString(value, "text/html").body.innerHTML);
+    });
   })
   return text;
 }
