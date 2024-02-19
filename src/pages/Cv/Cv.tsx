b52699578105e3b19bf508ed6eec90fc48e24b5e
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-19 15:54:08 +0200

    Fixing X icon to be just an icon.

--- src/pages/Cv/Cv.tsx
+++ src/pages/Cv/Cv.tsx
@@ -27,7 +27,7 @@ function Cv() {
       {loading ? (<LinearProgress />) : null}
       <div className='actions'>
         <HyperLink abbreviationOrIcon={Download} href={googleDocsDownloadLink}></HyperLink>
-        <X className="close" href="#"></X>
+        <HyperLink className="close" abbreviationOrIcon={X} href="#"></HyperLink>
       </div>
       <iframe
         name={"Cv"/*"https://docs.google.com/document/d/e/2PACX-1vSzSr4y3V7X7CZzLts4lhEUw0IN1baK4-uay6OdVYainASBQ7ccySqyVDOQjxAmBMAUUUnWxJ-Z4voa/pub?embedded=true"*/}
