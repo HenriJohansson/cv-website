7c579c1d8b2db8286bfc6ba65d81b7387edf58e8
Henri Johansson <henri.mm.johansson@gmail.com>
2024-02-26 10:13:40 +0200

    Mobile version for CV website.

--- src/pages/Cv/Cv.tsx
+++ src/pages/Cv/Cv.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
 import HyperLink from '@components/Links/HyperLink';
 import { Download } from '@components/Icons/Download';
@@ -8,14 +8,29 @@ import { X } from '@components/Icons/X';
 
 function Cv() {
   const [loading, setLoading] = useState(true);
+  const [source, setSource] = useState("https://docs.google.com/document/d/e/2PACX-1vSzSr4y3V7X7CZzLts4lhEUw0IN1baK4-uay6OdVYainASBQ7ccySqyVDOQjxAmBMAUUUnWxJ-Z4voa/pub?embedded=true");
+  const [disclaimer, setDisclaimer] = useState("");
+  const [downloadFull, setDownloadFull] = useState("");
   const loadingDone = () => {
     setLoading(false);
   }
-  const googleDocsDownloadLink = "https://docs.google.com/document/d/1Wn6mfjrgqCSfnwH_H8T4rL0XjOp88_zVKDWiBHWsiFE/export?format=pdf"
+  const googleDocsDownloadLink = "https://docs.google.com/document/d/1Wn6mfjrgqCSfnwH_H8T4rL0XjOp88_zVKDWiBHWsiFE/export?format=pdf";
 
   const windowDims = useWindowDimensions()
 
   const width: string = windowDims.width - (16*4) + "px"
+  useEffect(() => {
+    if(windowDims.width <= 570) {
+      setSource("https://docs.google.com/document/d/e/2PACX-1vTk_Te-1oBj1r7qZu7GYV4MvtDnX6CWXpkwo4YEjboZb0t1lCFcWskgytlytIM1G3TVH4TVQ46IzAXl/pub?embedded=true");
+      setDisclaimer("Mobile Version");
+      setDownloadFull("Download normal layout ->");
+    } else {
+      setSource("https://docs.google.com/document/d/e/2PACX-1vSzSr4y3V7X7CZzLts4lhEUw0IN1baK4-uay6OdVYainASBQ7ccySqyVDOQjxAmBMAUUUnWxJ-Z4voa/pub?embedded=true");
+      setDisclaimer("");
+      setDownloadFull("");
+    }
+  },[windowDims.width])
+
   const styleiFrame = {
     display: "block",
     height: "100%",
@@ -26,12 +41,14 @@ function Cv() {
     <div className="content">
       {loading ? (<LinearProgress />) : null}
       <div className='actions'>
+        {disclaimer ?
+        <><p id="disclaimer">{disclaimer}</p><p>{downloadFull}</p></> : null}
         <HyperLink abbreviationOrIcon={Download} href={googleDocsDownloadLink}></HyperLink>
         <HyperLink className="close" abbreviationOrIcon={X} href="#"></HyperLink>
       </div>
       <iframe
         name={"Cv"/*"https://docs.google.com/document/d/e/2PACX-1vSzSr4y3V7X7CZzLts4lhEUw0IN1baK4-uay6OdVYainASBQ7ccySqyVDOQjxAmBMAUUUnWxJ-Z4voa/pub?embedded=true"*/}
-        src="https://docs.google.com/document/d/e/2PACX-1vSzSr4y3V7X7CZzLts4lhEUw0IN1baK4-uay6OdVYainASBQ7ccySqyVDOQjxAmBMAUUUnWxJ-Z4voa/pub?embedded=true"
+        src={source}
         onLoad={loadingDone}
         style={styleiFrame}
         allow="picture-in-picture full">
